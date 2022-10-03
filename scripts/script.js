bc8dd3bb83b1d537a712040ace87a327b6985de6
TijnG1234 <112865386+TijnG1234@users.noreply.github.com>
2022-10-03 16:23:11 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,2 @@
-const menu = document.querySelector('nav h4');
-const menuLinks = document.querySelector('nav ul');
-const body = document.querySelector('body');
-
-
-const mobileMenu = () => {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-    body.classList.toggle('active');
-};
-
-menu.addEventListener('click', mobileMenu);
+// JavaScript Document
+console.log("hi");
